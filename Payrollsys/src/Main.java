58e22b7bd2d88785cfee798c38f308e308c0a4d5
gancoder <bambaolouis@gmail.com>
2025-07-09 22:11:50 +0800

    Delete Payrollsys directory

--- Payrollsys/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Payroll payroll = new Payroll(scanner);
-        payroll.processPayroll();
-    }
-}
